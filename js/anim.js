87886877d0d40c0048097e070c99c5ba6f01260a
Whoahbot <whoahbot@gmail.com>
2010-10-07 15:45:44 -0700

    More updates, got it reading from github wiki

--- js/anim.js
+++ js/anim.js
@@ -1,29 +1,32 @@
 (function() {
-  document.write("<canvas id='stars' width='940px' height='600px' style='background-color:black;'></canvas>")
+  document.write("<canvas id='stars' width='800px' height='550px' style='background-color:black;position:absolute;top:15px;left:15px'></canvas>")
   var canvas = document.getElementById('stars');
   var ctx = canvas.getContext('2d');
 
-  var y = (Math.floor(Math.random() * 130) + 15);
-  var x = (Math.floor(Math.random() * 580) + 15);
-  var ring = {x: x, y: y, size: 5};
+  ctx.font = "30pt Arial";
+  ctx.shadowColor = '#DDDDDD';
+  ctx.shadowOffsetX = '0';
+  ctx.shadowOffsetY = '2';
+  ctx.shadowBlur = '2';
+
+  var readme = document.getElementById('readme');
+  var h1s = readme.getElementsByTagName('h1');
+  var elements = [];
+  for (var i = 0; i < h1s.length; i++) {
+    var text = {title: h1s[i].innerHTML, y: (500 + (i * 50)), scale: 0.65, opacity: 1};
+    elements.push(text);
+  }
 
   setInterval(function() {
-    ctx.clearRect(0, 0, 600, 150);
-    ctx.beginPath();
-
-    if((ring.x + 1) == 130) {
-      ring.x -= Math.random();
-    } else {
-      ring.x += Math.random();
-    }
-
-    if((ring.y + 1) == 580) {
-      ring.y -= Math.random();
-    } else {
-      ring.y += Math.random();
-    }
-    ctx.arc(ring.x,ring.y,ring.size,0,Math.PI * 2,true);
-    ctx.strokeStyle = 'rgba(5,5,5,1)';
-    ctx.stroke();
-  }, 10);
+    ctx.clearRect(0,0,800,550);
+    for(var j = 0; j < elements.length; j++) {
+      ctx.save();
+      var text = elements[j];
+      ctx.fillStyle = "rgba(255,255,0," + text.opacity +")";
+      ctx.scale(1,text.scale);
+      ctx.fillText(text.title, 100, text.y);
+      text.y -= 2;
+      ctx.restore();
+    };
+  }, 50);
 })();
