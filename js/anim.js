b92165b41b31400673a14f2f78990265015f6c0a
Whoahbot <whoahbot@gmail.com>
2010-10-08 16:08:25 -0700

    Fixes, works in safari

--- js/anim.js
+++ js/anim.js
@@ -1,5 +1,5 @@
 (function() {
-  document.write("<canvas id='stars' width='800px' height='550px' style='background-color:black;position:absolute;top:15px;left:15px'></canvas>")
+  document.write("<div id='perspective' style='-webkit-perspective:500;background-color:black;'><canvas id='stars' width='800px' height='550px' style='-webkit-transform: rotateX(40deg)'></canvas></div>")
   var canvas = document.getElementById('stars');
   var ctx = canvas.getContext('2d');
 
@@ -17,13 +17,14 @@
     elements.push(text);
   }
 
+  //ctx.translate(-180, 0);
+
   setInterval(function() {
     ctx.clearRect(0,0,800,550);
     for(var j = 0; j < elements.length; j++) {
       ctx.save();
       var text = elements[j];
       ctx.fillStyle = "rgba(255,255,0," + text.opacity +")";
-      ctx.scale(1,text.scale);
       ctx.fillText(text.title, 100, text.y);
       text.y -= 2;
       ctx.restore();
